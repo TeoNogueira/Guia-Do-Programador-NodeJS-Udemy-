63e945e0664eb753e074d759e594a752ec891414
Teo Nogueira <teonogueira17@gmail.com>
2020-12-18 00:41:48 -0300

    Mysql Básico

--- NodeJS/EXPRESS/index.js
+++ NodeJS/EXPRESS/index.js
@@ -1,9 +1,58 @@
 // iniciando o express
+//TRABALHANDO COM ROTAS 
 
 const express = require("express"); // importando o express
 
 const app = express(); //iniciando o express, e passando essa inicialização para variável app
 
+// criando uma rota
+
+
+app.get("/", function(req, res){
+
+// resposta para o servidor
+res.send("Bem-vindo ao guia do programador")
+//res.send("bem-vindo ao sdasdadsa do programador") dica: não enviar mais de uma resposta para o client
+// lembrar sempre de enviar uma resposta apenas.1
+
+})
+// PARAMETROS NAO-OBRIGATORIOS
+app.get("/blog/:artigo?", function(req, res){
+let artigo = req.params.artigo
+if(artigo){
+
+res.send(`${artigo} ARTIGO EXISTENTE`)
+
+}
+else{res.send("<h4>Bem-vindo ao meu blog</h4> : guiadoprogramador")}
+
+
+})
+
+//parametros obrigatorios
+app.get("/vem/:nome/:empresa", function(req, res){
+  //REQUISIÇAO SÃO OS DADOS ENVIADOS PELO USUÁRIO
+  // resposta são as respostas enviadas para o usuário
+let nome = req.params.nome
+let empresa = req.params.empresa
+    res.send(`<h1>Oi: ${nome}, Bem-vindo ao nosso Blog! </h1><br><br>Empresa: ${empresa}`)
+
+
+
+})
+//QUERY PARAM
+app.get("/canal/youtube", function(req, res){
+let canal = req.query["canal"]
+
+if(canal) {
+
+
+    res.send(canal);
+}else{ res.send("Nenhum canal fornecido")}
+})
+
+
+//iniciando servidor
 
 app.listen(4000, function(erro){
 
@@ -15,11 +64,14 @@ if(erro){
 }
 
 })
+
+
+
 //suficiente para executar um servidor EXPRESS
+// para iniciar o servidor digite no terminal: node index.js
 
 
 
 
-
-
+//PARA INSTALAR O MONITORAMENTO DO SERVIDOR SEM PRECISAR TA REINICIALIZANDO ELE CONSTANTEMENTE USE: npm install nodemon -g
 
---
+++ NodeJS/mysqlcommit.js
@@ -0,0 +1,26 @@
+// comandos mysql
+
+
+//SHOW DATABASES; = PARA MOSTRAR DATABASES
+//CREATE DATABASE nomedadatabase;
+//USE nomedadatabase;
+
+// depois de usar o use, ficará logado numa database o
+/* CREATE TABLE usuarios(
+ nome VARCHAR(50), 
+ email VARCHAR(100),
+ idade INT(2)
+);*/ // DÊ UM CONTROL C E ENVIE PARA O TERMINAL QUE ESTÁ SENDO UTILIZADO O mysql
+
+// depois para verificar logado as tabelas da database digite: SHOW TABLES.
+
+// para ver as colunas dentro de usuarios digite: DESCRIBE usuarios
+
+//----------------------x------------------x-------------------x------------------x----------------
+
+/* para inserir dados use o comando: INSERT INTO usuarios(nome, email, idade) VALUES(
+
+"Teófilo Nogueira",
+"email@teste.com",
+30
+);*/
